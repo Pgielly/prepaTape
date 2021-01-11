c862b0ea24b96b9a2bf2c20724ed6b75fb8f6291
jperaudon <j.peraudon@it-students.fr>
2021-01-11 16:50:12 +0100

    Add display chocobos

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,9 @@
 let numBlocks;
 let board = document.querySelector('#boardgame');
+let score;
+let rand;
+let randImg;
+let block;
 
 document.querySelector('#startGame').addEventListener('click', () => {
     numBlocks = document.querySelector('#boardSize').value;
@@ -12,5 +16,18 @@ document.querySelector('#startGame').addEventListener('click', () => {
         block.style.width = `${sqrt}%`;
         block.style.height = `${sqrt}%`;
     })
-debugger;
+
+    setInterval(displayChoco, 3000);
+
 })
+
+function displayChoco() {
+    if (rand) {
+        block.style.backgroundImage = '';
+    }
+    rand = Math.floor(Math.random()*numBlocks) + 1;
+    block = document.querySelector(`[data-id='${rand}']`);
+    randImg = Math.floor(Math.random()*3) + 1;
+    block.style.backgroundImage = `URL('./public/img/choco${randImg}.jpg')`;
+}
+
